981d5d3f68f6f57cd2a70130b67ca38121efc117
Aline Campos <alineapp.docs2@gmail.com>
2026-05-21 19:24:09 -0300

    requirements

--- mapa_colaborativo/backend/main.py
+++ mapa_colaborativo/backend/main.py
@@ -67,9 +67,7 @@ oauth2_scheme = OAuth2PasswordBearer(
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=[
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],
 
     allow_credentials=True,
 
