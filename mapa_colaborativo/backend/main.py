4640098a7634df223b246bc46faeff26314a76f4
Aline Campos <alineapp.docs2@gmail.com>
2026-05-22 19:34:44 -0300

    alteracao banco de dados

--- mapa_colaborativo/backend/main.py
+++ mapa_colaborativo/backend/main.py
@@ -30,6 +30,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+from database import engine
+from models import Base
+
+Base.metadata.create_all(bind=engine)
 app.mount(
     "/static",
     StaticFiles(directory="../frontend"),
