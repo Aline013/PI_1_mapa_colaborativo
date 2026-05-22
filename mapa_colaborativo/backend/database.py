5952af56780c8f4c15acc8eb6dd98eadf83d2806
Aline Campos <alineapp.docs2@gmail.com>
2026-05-22 17:32:13 -0300

    mudanca local host

--- mapa_colaborativo/backend/database.py
+++ mapa_colaborativo/backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:p05Tgr35@localhost:5432/mapa_colaborativo"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
